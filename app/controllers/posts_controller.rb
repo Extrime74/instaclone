c0f22885a074b7b9b52a646711d2afd8dfd876db
Alexander Posukhovskiy <extrime74@gmail.com>
2024-06-30 17:14:43 +0300

    Added:
    Active storage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -65,6 +65,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :description, :keyswords)
+      params.require(:post).permit(:image, :title, :description, :keyswords)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
 
     validates :title, presence:true 
     validates :description, length: {maximum:500}
+    has_one_attached :image
+    validates_presence_of :image
 end
