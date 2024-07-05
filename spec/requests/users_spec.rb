ed72aa9c062fee19bacd73d8e34f0dbb9a7635f1
Alexander Posukhovskiy <extrime74@gmail.com>
2024-07-05 21:58:50 +0300

    Added:
    Users Requests test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,11 +1,16 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /show" do
-    it "returns http success" do
-      get "/users/show"
+RSpec.describe 'Users', type: :request do
+
+  describe 'GET /show' do
+    let(:user) { create(:user) }
+
+    it 'returns http success' do
+      get "/users/#{user.id}"
+      follow_redirect! # Follow the redirect caused by authentication
       expect(response).to have_http_status(:success)
     end
   end
-
 end
