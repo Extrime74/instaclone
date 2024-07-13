c8b8aaa68efb9f0cf8397181f2561ed181ab118a
Alexander Posukhovskiy <extrime74@gmail.com>
2024-07-13 16:02:05 +0300

    Fixed:
    Likes Controller and Tests

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,14 +3,14 @@
 class LikesController < ApplicationController
   def create
     @like = current_user.likes.new(like_params)
-    flash[:alert] = 'You already liked this post' unless @like.save
-    redirect_to root_path
+    @like.save
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @like = current_user.likes.find(params[:id])
     @like.destroy
-    redirect_to root_path
+    redirect_back(fallback_location: root_path)
   end
 
   def like_params
